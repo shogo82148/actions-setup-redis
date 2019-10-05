dbd39493cb1c8af5e5c2bd74c4613c30197fc117
Ichinose Shogo <shogo82148@gmail.com>
2019-10-05 15:42:50 +0900

    build redis

--- build-redis.sh
+++ build-redis.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
-echo "TODO: build redis"
+set -uex
+
+# download
+ROOT=$(cd "$(dirname "$0")" && pwd)
+REDIS_VERSION=$1
+: "${RUNNER_TEMP:=$ROOT/.work}"
+mkdir -p "$RUNNER_TEMP"
+curl -sSL "https://github.com/antirez/redis/archive/$REDIS_VERSION.tar.gz" -o "$RUNNER_TEMP/redis.tar.gz"
+
+# build
+cd "$RUNNER_TEMP"
+tar xzf redis.tar.gz
+cd "redis-$REDIS_VERSION"
+make
+
+mkdir -p "$RUNNER_TEMP/dist"
+make install PREFIX="$RUNNER_TEMP/dist"
+
+cd "$RUNNER_TEMP/dist"
+tar czf "../redis-$REDIS_VERSION.tar.gz" .
