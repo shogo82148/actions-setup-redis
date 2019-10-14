9346a3513e6b64523a78712ebd7a5c692313f41b
Ichinose Shogo <shogo82148@gmail.com>
2019-10-15 08:01:51 +0900

    run starter

--- src/setup-redis.ts
+++ src/setup-redis.ts
@@ -1,15 +1,51 @@
 import * as core from '@actions/core';
 import * as installer from './installer';
+import * as starter from '../src/starter';
 
 async function run() {
   try {
     const version = core.getInput('redis-version');
-    if (version) {
-      await installer.getRedis(version);
+    const port = parseInt(core.getInput('redis-port'));
+    const autoStart = parseBoolean(core.getInput('auto-start'));
+    if (!version) {
+      return;
+    }
+
+    const redisPath = await core.group('install redis', async () => {
+      return installer.getRedis(version);
+    });
+    if (autoStart) {
+      core.group('start redis', async () => {
+        const confDir = process.env['RUNNER_TEMP'] || '/tmp';
+        await starter.startRedis(confDir, redisPath, port);
+      });
     }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
+function parseBoolean(s: string): boolean {
+  switch (s) {
+    case 'y':
+    case 'Y':
+    case 'yes':
+    case 'Yes':
+    case 'YES':
+    case 'true':
+    case 'True':
+    case 'TRUE':
+      return true;
+    case 'n':
+    case 'N':
+    case 'no':
+    case 'No':
+    case 'NO':
+    case 'false':
+    case 'False':
+    case 'FALSE':
+      return false;
+  }
+  throw `invalid boolean value: ${s}`;
+}
 run();
