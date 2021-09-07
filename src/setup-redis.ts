ce67e9b30b5c0bcd644fef3dc7cbbbdb367d0db0
Ichinose Shogo <shogo82148@gmail.com>
2021-09-07 23:10:54 +0900

    configure tls certificates and keys

--- src/setup-redis.ts
+++ src/setup-redis.ts
@@ -9,8 +9,9 @@ async function run() {
     const required = {required: true};
     const version = core.getInput('redis-version', required);
     const port = parseInt(core.getInput('redis-port', required));
+    const tlsPort = parseInt(core.getInput('redis-tls-port', required));
     const autoStart = core.getBooleanInput('auto-start', required);
-    const conf = core.getInput('redis-conf');
+    const configure = core.getInput('redis-conf');
 
     const redisPath = await core.group('install redis', async () => {
       return installer.getRedis(version);
@@ -18,8 +19,14 @@ async function run() {
     if (autoStart) {
       core.group('start redis', async () => {
         const tempDir = process.env['RUNNER_TEMP'] || '/tmp';
-        const confDir = await fs.mkdtemp(tempDir + path.sep);
-        await starter.startRedis(confDir, redisPath, port, conf);
+        const confPath = await fs.mkdtemp(tempDir + path.sep);
+        await starter.startRedis({
+          confPath,
+          redisPath,
+          port,
+          tlsPort,
+          configure
+        });
       });
     }
   } catch (error) {
