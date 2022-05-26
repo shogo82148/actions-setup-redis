a8254fb769dc1ab220d5198ccef868479b458e69
Ichinose Shogo <shogo82148@gmail.com>
2022-05-26 22:00:20 +0900

    LDFLAGS=-rpath,'$ORIGIN/../lib'

--- build-redis.sh
+++ build-redis.sh
@@ -6,6 +6,9 @@ set -uex
 ROOT=$(cd "$(dirname "$0")" && pwd)
 REDIS_VERSION=$1
 : "${RUNNER_TEMP:=$ROOT/.work}"
+# shellcheck disable=SC2016
+export LDFLAGS=-rpath,'$ORIGIN/../lib'
+
 mkdir -p "$RUNNER_TEMP"
 curl -sSL "https://github.com/redis/redis/archive/$REDIS_VERSION.tar.gz" -o "$RUNNER_TEMP/redis.tar.gz"
 
