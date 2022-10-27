c1032d7dfb4e2ac4bb09744dd379521a822eefb2
Ichinose Shogo <shogo82148@gmail.com>
2022-10-27 09:46:09 +0900

    install only OpenSSL software

--- build-redis-with-openssl3.sh
+++ build-redis-with-openssl3.sh
@@ -40,7 +40,7 @@ echo "::group::build OpenSSL"
     cd "$RUNNER_TEMP/openssl-openssl-$OPENSSL_VERSION"
     ./Configure --prefix="$PREFIX" --openssldir="$PREFIX" --libdir=lib
     make "-j$JOBS"
-    make install
+    make install_sw install_ssldirs
 )
 echo "::endgroup::"
 
