93a65bd4c56009db1aaf0cefcc6f7a7458517e09
ICHINOSE Shogo <shogo82148@gmail.com>
2024-08-13 12:01:52 +0900

    test of eslint workflow

--- src/installer.ts
+++ src/installer.ts
@@ -25,6 +25,7 @@ interface Strategy {
   matrix: Matrix;
 }
 
+// FIXME
 interface Matrix {
   [key: string]: string[];
 }
@@ -52,6 +53,7 @@ export interface Redis {
   version: string;
 }
 
+// FIXME
 const minorVersions: { [key: string]: string[] } = {
   redis: ["7.4", "7.2", "7.0", "6.2", "6.0", "5.0", "4.0", "3.2", "3.0", "2.8"],
   valkey: ["7.2"],
