48e7f36cbcec6335e223cc5c51e6752717da494f
ICHINOSE Shogo <shogo82148@gmail.com>
2026-03-15 01:48:00 +0900

    Update ESLint configuration to include ignores for dist, lib, node_modules, and jest.config.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,6 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.strict, ...tseslint.configs.stylistic);
+export default tseslint.config(eslint.configs.recommended, {
+  ignores: ["dist/", "lib/", "node_modules/", "jest.config.js"],
+});
